10c0a9e2c4001513cbb13e7cf8c708716fd3b013
shaneadev <43152147+shaneadev@users.noreply.github.com>
2019-06-27 13:31:41 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext.commands import Bot
+import asyncio
+import time
+import os
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = "!")
+@client.event
+async def on_ready():
+	print("I am online now!")
+
+@client.event
+async def on_message(message):
+	if message.content.startswith('hello'):
+		msg = 'Hello (0.author.mention), how are you today?'.format(message)
+		await client.send_message(message.channel, msg)
+		
+client.run(os.getenv('TOKEN'))
