53892a0803b2248c107e73da2fa135bd412a39a0
shaneadev <43152147+shaneadev@users.noreply.github.com>
2019-06-27 14:00:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,16 +6,10 @@ import time
 import os
 
 Client = discord.Client()
-client = commands.Bot(command_prefix='!')
+client = commands.Bot(command_prefix='/')
 
 @client.event
 async def on_ready():
 	print("I am online now!")
-
-@client.event
-async def on_message(message):
-	if message.content.startswith('hello'):
-		msg = 'Hello (0.author.mention), how are you today?'.format(message)
-		await client.send_message(message.channel, msg)
 		
 client.run(os.getenv('TOKEN'))
