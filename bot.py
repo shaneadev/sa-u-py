99ec81635952a4e66b62cf388b3f32f50f298e07
shaneadev <43152147+shaneadev@users.noreply.github.com>
2019-06-27 14:04:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,5 +11,19 @@ client = commands.Bot(command_prefix='/')
 @client.event
 async def on_ready():
 	print("I am online now!")
+	
+## start hello command
+def hello_function(message, client, args):
+    try:
+        return 'Hello {}, Argument One: {}'.format(message.author, args[0])
+    except Exception as e:
+        return e
+ch.add_command({
+    'trigger': '/hello',
+    'function': hello_function,
+    'args_num': 1,
+    'args_name': ['string'],
+    'description': 'Will respond hello to the caller'
+})
 		
 client.run(os.getenv('TOKEN'))
